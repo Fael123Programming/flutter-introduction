bcb0623c9d3b164c951ba7e76d2f8b1b017aed37
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-04-06 14:54:02 -0300

    new files

--- intermediate/bmi_calculator/lib/home.dart
+++ intermediate/bmi_calculator/lib/home.dart
@@ -22,70 +22,91 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         // backgroundColor: Colors.red,
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-            width: 80,
-            height: 80,
-            child: const Image(
-              image: AssetImage("images/doctor.png"),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: TextField(
-              controller: heightController, //Linking a controller for it.
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Height",
-                suffixText: "centimeters",
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                width: 80,
+                height: 80,
+                child: const Image(
+                  image: AssetImage("images/doctor.png"),
+                ),
               ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 10, 20, 30),
-            child: TextField(
-              controller: weightController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Weight",
-                suffixText: "kilograms",
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                  controller: heightController, //Linking a controller for it.
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Height",
+                    suffixText: "centimeters",
+                  ),
+                ),
               ),
-            ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+                child: TextField(
+                  controller: weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Weight",
+                    suffixText: "kilograms",
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  child:
+                      const Text("Calculate", style: TextStyle(fontSize: 20)),
+                  onPressed: () {
+                    String heightStr = heightController.text.toString();
+                    String weightStr = weightController.text.toString();
+                    String report;
+                    if (heightStr.isEmpty || weightStr.isEmpty) {
+                      report = "Please, enter valid values";
+                    } else {
+                      double height = double.parse(heightStr) / 100;
+                      double weight = double.parse(weightStr);
+                      if (height <= 0 || weight <= 0) {
+                        report = "Please, enter valid values";
+                      } else {
+                        double bmi = weight / (height * height);
+                        report =
+                            "Your BMI is ${bmi.toStringAsFixed(2)}\nYou are ";
+                        if (bmi < 18.5) {
+                          report += "underweight";
+                        } else if (bmi < 25) {
+                          report += "normal";
+                        } else if (bmi < 30) {
+                          report += "overweight";
+                        } else if (bmi < 35) {
+                          report += "grade I obesity";
+                        } else if (bmi < 40) {
+                          report += "grade II obesity (severe)";
+                        } else {
+                          report += "grade III obesity (morbid)";
+                        }
+                      }
+                    }
+                    heightController.text = "";
+                    weightController.text = "";
+                    FocusScope.of(context).unfocus();
+                    setState(() {
+                      result = report;
+                    });
+                  }),
+              Container(
+                margin: const EdgeInsets.only(top: 50),
+                child: Text(result,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ],
           ),
-          ElevatedButton(
-              child: const Text("Calculate", style: TextStyle(fontSize: 20)),
-              onPressed: () {
-                double height = double.parse(heightController.text) / 100;
-                double weight = double.parse(weightController.text);
-                String report = "";
-                if (height <= 0 || weight <= 0) {
-                  report = "Please, enter valid values";
-                } else {
-                  double bmi = weight / (height * height);
-                  report = "Your BMI is ${bmi.toStringAsFixed(2)}\nYou are ";
-                  if (bmi < 18.5) {
-                    report += "underweight";
-                  } else if (bmi < 25) {
-                    report += "normal";
-                  } else if (bmi < 30) {
-                    report += "overweight";
-                  } else {
-                    report += "obese";
-                  }
-                }
-                setState(() {
-                  result = report;
-                });
-              }),
-          Container(
-            margin: const EdgeInsets.only(top: 50),
-            child: Text(result,
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
-        ],
+        ),
       ),
     );
   }
