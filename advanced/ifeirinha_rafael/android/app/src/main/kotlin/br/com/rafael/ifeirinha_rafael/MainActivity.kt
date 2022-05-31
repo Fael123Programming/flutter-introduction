c884055636ad1cc76dfd8dca52368ea6b6ac55dd
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-05-31 11:13:57 -0300

    splash, sign-in and sign-up screens

---
+++ advanced/ifeirinha_rafael/android/app/src/main/kotlin/br/com/rafael/ifeirinha_rafael/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.rafael.ifeirinha_rafael
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ advanced/ifeirinha_rafael/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:ifeirinha_rafael/screens/initialization/sign_in_screen.dart';
+import 'package:ifeirinha_rafael/screens/initialization/sign_up_screen.dart';
+import 'package:ifeirinha_rafael/screens/initialization/splash_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+          primary: Color(0xFFFFB20F),
+          onPrimary: Color(0xFF001514),
+          secondary: Color(0xFF001514),
+          onSecondary: Color(0x0fffffff),
+          background: Color(0x0fffffff),
+          onBackground: Color(0xFF001514),
+          error: Color(0XFFCD3728),
+          onError: Color(0x0fffffff),
+          surface: Color(0x0fffffff),
+          onSurface: Color(0xFF001514),
+          brightness: Brightness.light,
+        ),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 96,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -1.5,
+            color: Color(0xFF001514),
+          ),
+          headline2: TextStyle(
+            fontSize: 60,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -0.5,
+            color: Color(0xFF001514),
+          ),
+          headline3: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+            color: Color(0xFF001514),
+          ),
+          headline4: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+            color: Color(0xFF001514),
+          ),
+          headline5: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+            color: Color(0xFF001514),
+          ),
+          headline6: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+            color: Color(0xFF001514),
+          ),
+          subtitle1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+            color: Color(0xFF001514),
+          ),
+          subtitle2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.1,
+            color: Color(0xFF001514),
+          ),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+            color: Color(0xFF001514),
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+            color: Color(0xFF001514),
+          ),
+          button: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1.25,
+            color: Color(0xFF001514),
+          ),
+          caption: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.4,
+            color: Color(0xFF001514),
+          ),
+          overline: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+            color: Color(0xFF001514),
+          ),
+        ),
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const SplashScreen(),
+        "/sign_in_screen": (context) => const SignInScreen(),
+        "/sign_up_screen": (context) => const SignUpScreen()
+      },
+    ),
+  );
+//Theme.of(context).colorScheme
+//Theme.of(context).textTheme
+}
