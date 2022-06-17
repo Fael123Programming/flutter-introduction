aa5c134c0610079aa2c51bc20578f958dedc0ca0
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-06-17 15:33:00 -0300

    new changes

---
+++ advanced/ifeirinha_rafael/lib/models/forms/validation_code_form.dart
@@ -0,0 +1,315 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ifeirinha_rafael/models/screen_sizer/screen_percentage.dart';
+import 'package:ifeirinha_rafael/models/screen_sizer/screen_sizer.dart';
+
+class ValidationCodeForm extends StatefulWidget {
+  const ValidationCodeForm({Key? key}) : super(key: key);
+
+  @override
+  _ValidationCodeFormState createState() => _ValidationCodeFormState();
+}
+
+class _ValidationCodeFormState extends State<ValidationCodeForm> {
+  final textFormField1Controller = TextEditingController();
+  final textFormField2Controller = TextEditingController();
+  final textFormField3Controller = TextEditingController();
+  final textFormField4Controller = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  late FocusNode textFormField1FocusNode;
+  late FocusNode textFormField2FocusNode;
+  late FocusNode textFormField3FocusNode;
+  late FocusNode textFormField4FocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    textFormField1FocusNode = FocusNode();
+    textFormField2FocusNode = FocusNode();
+    textFormField3FocusNode = FocusNode();
+    textFormField4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    textFormField2FocusNode.dispose();
+    textFormField3FocusNode.dispose();
+    textFormField4FocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenSizer screenSizer = ScreenSizer(context);
+    screenSizer.adjustHeightIfDeviceIsiOS();
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: screenSizer.convertToDeviceScreenHeight(
+                screenPercentage: ScreenPercentage.marginTopFormContainer,
+              ),
+            ),
+            child: Form(
+              key: formKey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    width: 50,
+                    height: 100,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          if (textFormField2Controller.text.isEmpty) {
+                            textFormField2FocusNode.requestFocus();
+                          } else if (textFormField3Controller.text.isEmpty) {
+                            textFormField3FocusNode.requestFocus();
+                          } else if (textFormField4Controller.text.isEmpty) {
+                            textFormField4FocusNode.requestFocus();
+                          } else {
+                            textFormField2FocusNode.requestFocus();
+                          }
+                        }
+                      },
+                      autofocus: true,
+                      focusNode: textFormField1FocusNode,
+                      style: GoogleFonts.roboto(fontSize: 20),
+                      maxLength: 1,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        errorBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                        counterText: '',
+                      ),
+                      controller: textFormField1Controller,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    width: 50,
+                    height: 100,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          if (textFormField1Controller.text.isEmpty) {
+                            textFormField1FocusNode.requestFocus();
+                          } else if (textFormField3Controller.text.isEmpty) {
+                            textFormField3FocusNode.requestFocus();
+                          } else if (textFormField4Controller.text.isEmpty) {
+                            textFormField4FocusNode.requestFocus();
+                          } else {
+                            textFormField3FocusNode.requestFocus();
+                          }
+                        }
+                      },
+                      focusNode: textFormField2FocusNode,
+                      style: GoogleFonts.roboto(fontSize: 20),
+                      maxLength: 1,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        errorBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                        counterText: '',
+                      ),
+                      controller: textFormField2Controller,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    width: 50,
+                    height: 100,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          if (textFormField1Controller.text.isEmpty) {
+                            textFormField1FocusNode.requestFocus();
+                          } else if (textFormField2Controller.text.isEmpty) {
+                            textFormField2FocusNode.requestFocus();
+                          } else {
+                            textFormField4FocusNode.requestFocus();
+                          }
+                        }
+                      },
+                      focusNode: textFormField3FocusNode,
+                      style: GoogleFonts.roboto(fontSize: 20),
+                      maxLength: 1,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        errorBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                        counterText: '',
+                      ),
+                      controller: textFormField3Controller,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    width: 50,
+                    height: 100,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          if (textFormField1Controller.text.isEmpty) {
+                            textFormField1FocusNode.requestFocus();
+                          } else if (textFormField2Controller.text.isEmpty) {
+                            textFormField2FocusNode.requestFocus();
+                          } else if (textFormField3Controller.text.isEmpty) {
+                            textFormField3FocusNode.requestFocus();
+                          } else {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          }
+                        }
+                      },
+                      focusNode: textFormField4FocusNode,
+                      style: GoogleFonts.roboto(fontSize: 20),
+                      maxLength: 1,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        errorBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                        counterText: '',
+                      ),
+                      controller: textFormField4Controller,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: screenSizer.convertToDeviceScreenHeight(
+                screenPercentage: ScreenPercentage.bigButtonMarginTop,
+              ),
+            ),
+            height: screenSizer.convertToDeviceScreenHeight(
+              screenPercentage: ScreenPercentage.bigButtonHeight,
+            ),
+            width: screenSizer.convertToDeviceScreenWidth(
+              screenPercentage: ScreenPercentage.bigButtonWidth,
+            ),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).colorScheme.primary,
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      screenSizer.convertToDeviceScreenWidth(
+                        screenPercentage: ScreenPercentage.bigButtonRadius,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                FocusManager.instance.primaryFocus!.unfocus();
+                String code = getCode();
+                if (isCodeValid(code)) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Continuar: $code'),
+                      duration: const Duration(seconds: 1),
+                    ),
+                  );
+                } else {
+                  cleanForm();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Código inválido'),
+                      duration: Duration(seconds: 1),
+                    ),
+                  );
+                  // textFormField1FocusNode.requestFocus();
+                }
+              },
+              child: Text(
+                'Continuar',
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: screenSizer.convertToDeviceScreenHeight(
+                screenPercentage: ScreenPercentage.bigButtonMarginTop,
+              ),
+            ),
+            height: screenSizer.convertToDeviceScreenHeight(
+              screenPercentage: ScreenPercentage.bigButtonHeight,
+            ),
+            width: screenSizer.convertToDeviceScreenWidth(
+              screenPercentage: ScreenPercentage.bigButtonWidthSpecial,
+            ),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).colorScheme.primary,
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      screenSizer.convertToDeviceScreenWidth(
+                        screenPercentage: ScreenPercentage.bigButtonRadius,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Código reenviado com sucesso'),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+              child: Text(
+                'Reenviar Código',
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isCodeValid(String code) {
+    return !(code.length < 4 || code.contains('.'));
+  }
+
+  void cleanForm() {
+    textFormField1Controller.text = '';
+    textFormField2Controller.text = '';
+    textFormField3Controller.text = '';
+    textFormField4Controller.text = '';
+  }
+
+  String getCode() {
+    return textFormField1Controller.text +
+        textFormField2Controller.text +
+        textFormField3Controller.text +
+        textFormField4Controller.text;
+  }
+}
--- advanced/ifeirinha_rafael/lib/screens/validation_code_screen.dart
+++ advanced/ifeirinha_rafael/lib/screens/validation_code_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ifeirinha_rafael/models/screen_sizer/screen_percentage.dart';
 import 'package:ifeirinha_rafael/models/screen_sizer/screen_sizer.dart';
+import 'package:ifeirinha_rafael/models/forms/validation_code_form.dart';
 
 class ValidationCodeScreen extends StatefulWidget {
   const ValidationCodeScreen({Key? key}) : super(key: key);
@@ -11,12 +12,17 @@ class ValidationCodeScreen extends StatefulWidget {
 }
 
 class _ValidationCodeScreenState extends State<ValidationCodeScreen> {
+  final textFormField1Controller = TextEditingController();
+  final textFormField2Controller = TextEditingController();
+  final textFormField3Controller = TextEditingController();
+  final textFormField4Controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     ScreenSizer screenSizer = ScreenSizer(context);
     screenSizer.adjustHeightIfDeviceIsiOS();
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(
@@ -27,138 +33,45 @@ class _ValidationCodeScreenState extends State<ValidationCodeScreen> {
           ),
         ),
       ),
-      body: GestureDetector(
-        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.only(
-                  top: screenSizer.convertToDeviceScreenHeight(
-                    screenPercentage: ScreenPercentage.marginTopContainerTitle,
-                  ),
-                ),
-                child: Text(
-                  'Código de Validação',
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: screenSizer.convertToDeviceScreenHeight(
-                    screenPercentage: ScreenPercentage.marginTopContainerTitle,
-                  ),
-                ),
-                child: Text(
-                  'Digite o código de 4 dígitos que foi enviado para seu e-mail',
-                  style: GoogleFonts.openSans(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 11,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: screenSizer.convertToDeviceScreenHeight(
-                    screenPercentage: ScreenPercentage.marginTopFormContainer,
-                  ),
-                ),
-                child: Row(),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: screenSizer.convertToDeviceScreenHeight(
-                    screenPercentage: ScreenPercentage.bigButtonMarginTop,
-                  ),
-                ),
-                height: screenSizer.convertToDeviceScreenHeight(
-                  screenPercentage: ScreenPercentage.bigButtonHeight,
-                ),
-                width: screenSizer.convertToDeviceScreenWidth(
-                  screenPercentage: ScreenPercentage.bigButtonWidth,
-                ),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Theme.of(context).colorScheme.primary,
-                    ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                          screenSizer.convertToDeviceScreenWidth(
-                            screenPercentage: ScreenPercentage.bigButtonRadius,
-                          ),
-                        ),
-                      ),
+      body: SingleChildScrollView(
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    top: screenSizer.convertToDeviceScreenHeight(
+                      screenPercentage: ScreenPercentage.marginTopContainerTitle,
                     ),
                   ),
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Continuar'),
-                        duration: Duration(seconds: 1),
-                      ),
-                    );
-                  },
                   child: Text(
-                    'Continuar',
+                    'Código de Validação',
                     style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30,
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: screenSizer.convertToDeviceScreenHeight(
-                    screenPercentage: ScreenPercentage.bigButtonMarginTop,
-                  ),
-                ),
-                height: screenSizer.convertToDeviceScreenHeight(
-                  screenPercentage: ScreenPercentage.bigButtonHeight,
-                ),
-                width: screenSizer.convertToDeviceScreenWidth(
-                  screenPercentage: ScreenPercentage.bigButtonWidthSpecial,
-                ),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Theme.of(context).colorScheme.primary,
-                    ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                          screenSizer.convertToDeviceScreenWidth(
-                            screenPercentage: ScreenPercentage.bigButtonRadius,
-                          ),
-                        ),
-                      ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: screenSizer.convertToDeviceScreenHeight(
+                      screenPercentage: ScreenPercentage.marginTopContainerTitle,
                     ),
                   ),
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Reenviar código'),
-                        duration: Duration(seconds: 1),
-                      ),
-                    );
-                  },
                   child: Text(
-                    'Reenviar Código',
-                    style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20,
+                    'Digite o código de 4 dígitos que foi enviado para seu e-mail',
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 11,
+                      color: Colors.grey,
                     ),
                   ),
                 ),
-              ),
-            ],
+                const ValidationCodeForm(),
+              ],
+            ),
           ),
         ),
       ),
